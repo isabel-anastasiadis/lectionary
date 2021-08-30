9cc5f19475abbcb09ed5643dd36e5ef236e5fc8a
Isabel Anastasiadis <anastasiadis.isabel@gmail.com>
2021-08-30 16:15:10 +1200

    fixed up the formatting

--- jupyter/run.ps1
+++ jupyter/run.ps1
@@ -1,3 +1,1 @@
-docker run --rm --name lectionary-jupyter -p 8888:8888 -v c:\data\Lectionary\jupyter:/home/jovyan/work jupyter/base-notebook;
-
-docker kill lectionary-jupyter;
+docker run --rm --name lectionary-jupyter -p 8888:8888 -v ${PWD}:/home/jovyan/work jupyter/base-notebook
