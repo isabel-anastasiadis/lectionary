443c3ff9fcd0bffcbe7103f1dbf4a7b103811b4b
Isabel Anastasiadis <anastasiadis.isabel@gmail.com>
2021-10-26 12:52:15 +1300

    Added navigation

--- new/src/App.tsx
+++ new/src/App.tsx
@@ -1,12 +1,19 @@
 
 import Header from './header/Header';
 import ReadingsPage from './readings/ReadingsPage';
+import AboutPage from './about/AboutPage';
+import {Route, Switch} from 'react-router-dom';
+import SettingsPage from './settings/SettingsPage';
 
 const App = () => {
   return (
     <>
       <Header/>
-      <ReadingsPage/>
+      <Switch>
+        <Route exact path="/" component={ReadingsPage}/>
+        <Route path="/about" component={AboutPage}/>
+        <Route path="/settings" component={SettingsPage}/>
+      </Switch>
     </>
   );
 }
--- new/src/header/Header.tsx
+++ new/src/header/Header.tsx
@@ -1,10 +1,23 @@
-interface HeaderProps {
-}
+import { NavLink } from "react-router-dom";
 
-const Header = (props: HeaderProps) => {
-    return(
-        <div><h1>Header!!</h1></div>
-    )
-}
+const Header = () => {
+  const activeStyle = { color: "#F15B2A" };
+
+  return (
+    <nav>
+      <NavLink to="/" activeStyle={activeStyle} exact>
+        Home
+      </NavLink>{" "}
+      {" | "}
+      <NavLink to="/about" activeStyle={activeStyle}>
+        About
+      </NavLink>
+      {" | "}
+      <NavLink to="/settings" activeStyle={activeStyle}>
+        Settings
+      </NavLink>
+    </nav>
+  );
+};
 
 export default Header;
