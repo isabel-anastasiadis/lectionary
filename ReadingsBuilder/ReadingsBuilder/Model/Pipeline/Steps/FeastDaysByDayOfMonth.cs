ba29d5c5893d7af32f241056b84cc4ab50724b69
Isabel Anastasiadis <anastasiadis.isabel@gmail.com>
2021-12-28 18:00:18 +1300

    Data is now being generated correctly

---
+++ ReadingsBuilder/ReadingsBuilder/Model/Pipeline/Steps/FeastDaysByDayOfMonth.cs
@@ -0,0 +1,22 @@
+﻿
+using ReadingsBuilder.Model.Data;
+using ReadingsBuilder.Model.Pipeline.DTOs;
+
+namespace ReadingsBuilder.Model.Pipeline.Steps
+{
+    public class FeastDaysByDayOfMonth : BaseStep, IStep
+    {
+        public FeastDaysByDayOfMonth(IRuleApplier ruleApplier, IAllDataFactory dataFactory) : base(ruleApplier, dataFactory)
+        {
+        }
+
+        public int Order => 9;
+
+        protected override string RuleSetName => "FeastDaysByDayOfMonth.cs";
+
+        public PipelineWorkingResult RunStep(PipelineWorkingResult workingResult)
+        {
+            return ApplyRulesByDayOfMonth(workingResult);
+        }
+    }
+}
