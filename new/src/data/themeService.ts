2e748291e4d0389114e18f6022716d574c2e5db5
Isabel Anastasiadis <anastasiadis.isabel@gmail.com>
2023-01-06 09:05:09 +1300

    chore: deleted the 'new' folder (the app now lives in 'website')

--- new/src/data/themeService.ts
+++
@@ -1,8 +0,0 @@
-import { Theme } from "./interfaces";
-
-export function pickDefaultTheme(): Theme {
-    const now = new Date();
-    const hours = now.getHours();
-    var morning = hours >= 0 && hours <= 12;
-    return morning ? Theme.MORNING : Theme.EVENING;
-  };
