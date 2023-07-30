11aeca9ab873f2a9f8485d1a31722501b7e42a6d
Isabel Anastasiadis <anastasiadis.isabel@gmail.com>
2023-07-30 18:36:30 +1200

    refactor: pulled input settings into consts so they can be reused by tests

---
+++ ReadingsBuilder/ReadingsBuilder/Inputs.cs
@@ -0,0 +1,34 @@
+﻿
+
+using ReadingsBuilder.Pipeline;
+
+namespace ReadingsBuilder
+{
+    public class Inputs
+    {
+
+        public static readonly Input FOR_2021_TO_2022 = new()
+        {
+            StartDate = new DateOnly(2021, 11, 28),
+            EndDate = new DateOnly(2022, 10, 30), // Haven't done Transfiguration of the Beloved Son and beyond for set feastival days.  Otherwise done until 4th Sun before Advent.
+            FifthSundayAfterEpiphany = new DateOnly(2022, 2, 6),
+            AshWednesday = new DateOnly(2022, 3, 2),
+            PalmSunday = new DateOnly(2022, 4, 10),
+            FirstSundayOfAdvent = new DateOnly(2022, 11, 27),
+            // haven't worked out the rule for these so you just have to check
+            OrdinaryTimePsalmsSecondChunkStartingIndex = 43  // 0 based index
+        };
+
+        public static readonly Input FOR_2022_TO_2023 = new()
+        {
+            StartDate = new DateOnly(2022, 11, 27), // first sun of advent
+            EndDate = new DateOnly(2023, 11, 4), // Sat before 4th Sun of Advent.  Haven't done Transfiguration of the Beloved Son and beyond for set feastival days.  Otherwise done until 4th Sun before Advent.
+            FifthSundayAfterEpiphany = new DateOnly(2023, 2, 5),
+            AshWednesday = new DateOnly(2023, 2, 22),
+            PalmSunday = new DateOnly(2023, 4, 2),
+            FirstSundayOfAdvent = new DateOnly(2023, 12, 3),
+            // haven't worked out the rule for these so you just have to check
+            OrdinaryTimePsalmsSecondChunkStartingIndex = 43  // 0 based index
+        };
+    }
+}
--- ReadingsBuilder/ReadingsBuilder/Program.cs
+++ ReadingsBuilder/ReadingsBuilder/Program.cs
@@ -20,20 +20,10 @@ Console.WriteLine("Hello, World!");
 var pipeline = DependencyInjector.GetInstance<IPipeline>();
 var resultWriter = DependencyInjector.GetInstance<IResultWriter>();
 
-// previous years results (for regression testing)
+// previous years results (for regression detection)
 var previousYearsInputs = new List<Input>() 
 {
-    new Input
-    {
-        StartDate = new DateOnly(2021, 11, 28),
-        EndDate = new DateOnly(2022, 10, 30), // Haven't done Transfiguration of the Beloved Son and beyond for set feastival days.  Otherwise done until 4th Sun before Advent.
-        FifthSundayAfterEpiphany = new DateOnly(2022, 2, 6),
-        AshWednesday = new DateOnly(2022, 3, 2),
-        PalmSunday = new DateOnly(2022, 4, 10),
-        FirstSundayOfAdvent = new DateOnly(2022, 11, 27),
-        // haven't worked out the rule for these so you just have to check
-        OrdinaryTimePsalmsSecondChunkStartingIndex = 43  // 0 based index
-    }
+    Inputs.FOR_2021_TO_2022
 };
 
 File.Delete(Path.Combine(ResultWriter.DefaultFilePath, "PreviousResults.csv"));
@@ -45,19 +35,7 @@ foreach (var previousYearsInput in previousYearsInputs)
 }
 
 
-// this years result
-var input = new Input 
-{ 
-    StartDate = new DateOnly(2022, 11, 27), // first sun of advent
-    EndDate = new DateOnly(2023, 11, 4), // Sat before 4th Sun of Advent.  Haven't done Transfiguration of the Beloved Son and beyond for set feastival days.  Otherwise done until 4th Sun before Advent.
-    FifthSundayAfterEpiphany = new DateOnly(2023, 2, 5),
-    AshWednesday = new DateOnly(2023, 2, 22),
-    PalmSunday = new DateOnly(2023, 4, 2),
-    FirstSundayOfAdvent = new DateOnly(2023, 12, 3),
-    // haven't worked out the rule for these so you just have to check
-    OrdinaryTimePsalmsSecondChunkStartingIndex = 43  // 0 based index
-};
-var result = pipeline.Run(input);
+var result = pipeline.Run(Inputs.FOR_2022_TO_2023);
 resultWriter.WriteResult(result, "Result.csv", overwrite: true, logToConsole: true);
 
 // copy to the processing folder
